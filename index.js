95673e3efbdcfdaa64c5cb25ac77788057b5ccce
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-11-01 00:25:17 +0200

    step10

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 const chartData = {
   type: 'spline',
+  color: '#739fe6',
   dataPoints: [
     { label: 'jaanuar', y: 10 },
     { label: 'veebruar', y: 15 },
@@ -10,11 +11,17 @@ const chartData = {
   ],
 };
 
+const iteratingChartData = {
+  type: 'spline',
+  color: '#e68a73',
+  dataPoints: [],
+};
+
 const chart = new CanvasJS.Chart('chartContainer', {
   title: {
     text: 'Minu graafik',
   },
-  data: [chartData],
+  data: [chartData, iteratingChartData],
 });
 
 chart.render();
@@ -28,7 +35,7 @@ const addToChart = (newLabel, newY) => {
 
     chartData.dataPoints.push({ label, y });
   } else {
-    chartData.dataPoints.push({ label: newLabel, y: newY });
+    iteratingChartData.dataPoints.push({ label: newLabel, y: newY });
   }
 
   updateTitle();
@@ -59,7 +66,10 @@ const colorLastPoint = () => {
 
 const randomIterator = () => {
   setInterval(() => {
-    addToChart(`punkt: ${chartData.dataPoints.length + 1}`, Math.floor(Math.random() * 256));
+    addToChart(
+      `punkt: ${iteratingChartData.dataPoints.length + 1}`,
+      Math.floor(Math.random() * 256)
+    );
   }, 1000);
 };
 
