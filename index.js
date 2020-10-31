3b11028cd1291870d58cb3f3ad7597afc5ca32b4
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-10-31 23:40:27 +0200

    step4

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 /* eslint-disable no-undef */
+const chartData = {
+  type: 'column',
+  dataPoints: [
+    { label: 'jaanuar', y: 10 },
+    { label: 'veebruar', y: 15 },
+    { label: 'märts', y: 25 },
+    { label: 'aprill', y: 30 },
+    { label: 'mai', y: 28 },
+  ],
+};
+
+/**
+ * Lisada ül kus proovida erinevaid charti tüüpe: doughnut, line splineArea, bar.
+ * Ülesandes ka muuta andmeid ja lisada dataPoint.
+ */
 
 const chart = new CanvasJS.Chart('chartContainer', {
   title: {
     text: 'Minu graafik',
   },
-  data: [],
+  data: [chartData],
 });
 
 chart.render();
